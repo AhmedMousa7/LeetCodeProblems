b867ad99557665bd4e230badab42b8ce45dc76d6
ahmedmousa <ahmed.mousa@areebgroup.com>
2020-12-05 21:50:10 +0200

    new solutions

---
+++ src/IntToRoman.kt
@@ -0,0 +1,41 @@
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
+
+@ExperimentalTime
+fun main(){
+    val (value, duration) = measureTimedValue {
+        intToRoman(65)
+    }
+    println(value)
+    println(duration)
+}
+
+fun intToRoman(num: Int): String {
+    var runningNum = num
+    val stringBuilder = StringBuilder()
+    for(digit in DIGIT.values()){
+        val divs = runningNum / digit.value
+        repeat(divs){
+            stringBuilder.append(digit.symbol)
+        }
+        runningNum -= digit.value * divs
+        if (runningNum == 0) break
+    }
+    return stringBuilder.toString()
+}
+
+enum class DIGIT(val value: Int, val symbol: String){
+    M(1000, "M"),
+    CM(900, "CM"),
+    D(500, "D"),
+    CD(400, "CD"),
+    C(100, "C"),
+    XC(90, "XC"),
+    L(50, "L"),
+    XL(40, "XL"),
+    X(10, "X"),
+    IX(9, "IX"),
+    V(5, "V"),
+    IV(4, "IV"),
+    I(1, "I")
+}
---
+++ src/ReverseInteger.kt
@@ -0,0 +1,24 @@
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
+
+@ExperimentalTime
+fun main(){
+    val (value, duration) = measureTimedValue {
+        reverse(1534236469)
+    }
+    println(value)
+    println(duration)
+}
+
+fun reverse(x: Int): Int {
+    var num = x
+    var reversed: Long = 0
+
+    while (num != 0) {
+        val digit = num % 10
+        reversed = reversed * 10 + digit
+        num /= 10
+    }
+
+    return if (reversed > Int.MAX_VALUE || reversed < Int.MIN_VALUE) 0 else reversed.toInt()
+}
